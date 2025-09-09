dff1d1eeeb95f688dbea0571b3797feaacc662d6
Nhi Tran <nhigt@uci.edu>
2025-09-09 01:49:40 -0700

    doesn't evaluate more than a single pair of number at once

--- calculator/main.js
+++ calculator/main.js
@@ -6,61 +6,83 @@ let equalBtn = document.getElementById("equal");
 let clearBtn = document.getElementById("clear");
 let outputDisplay = document.getElementById("output");
 
+let btnList = document.querySelectorAll("button");
 let digitList = document.querySelectorAll(".digit");
 let operatorList = document.querySelectorAll(".operator");
 
-let num1 = 0;
-let num2 = 0;
-let operator = "";
+let num1;
+let num2;
+let operator = [];
 
 clearBtn.addEventListener("click", clearOutput);
-
-digitList.forEach((item) => {
-    item.addEventListener("click", () => setOutput(item.textContent));
-});
+digitList.forEach((item) => { item.addEventListener("click", () => setOutput(item.textContent)); });
 
 operatorList.forEach((item) => {
     item.addEventListener("click", () => {
-        let itemContent = item.textContent;
-        setOperator(itemContent);
+        let content = item.textContent;
+
+        if (content != "=") {
+            if (!operator[0]) {
+                setOutput(content);
+                operator[0] = content;
+            }
+            else {
+                operator[1] = content;
+                evaluteOutput();
+                setOutput(content);
+                // operator.shift();
+            }
+        } else {
+            evaluteOutput();
+        }
     })
 });
 
 function clearOutput() {
-    outputDisplay.textContent = 0;
+    outputDisplay.textContent = "0";
+    num1 = undefined;
+    num2 = undefined;
+    operator = [];
 }
 
-function add(a, b) {
-    return a + b;
-}
-
-function subtract(a, b) {
-    return a - b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a, b) {
-    return a / b;
-}
-
-function operate(operator, num1, num2) {
-
-}
+function add(a, b) { return a + b; }
+function subtract(a, b) { return a - b; }
+function multiply(a, b) { return a * b; }
+function divide(a, b) { return a / b; }
 
 function setOutput(str) {
-    num1 = Number(str);
-
-    if (outputDisplay.textContent == "0") {
-        outputDisplay.textContent = num1;
+    if (outputDisplay.textContent === "0") {
+        outputDisplay.textContent = str;
     } else {
-        outputDisplay.textContent += num1;
+        outputDisplay.textContent += str;
     }
 }
 
-function setOperator(input) {
-    operator = input;
-    outputDisplay.textContent += operator;
+function evaluteOutput() {
+    let arr = outputDisplay.textContent.split(operator[0]);
+    console.log(arr);
+    num1 = Number(arr[0]);
+    num2 = Number(arr[1]);
+
+    outputDisplay.textContent = "0";
+    console.log(num1, num2)
+
+    if (num2 == NaN) { return; }
+
+    if (operator[0] == "+") {
+        num1 = add(num1, num2);
+        setOutput(num1);
+    } else if (operator[0] == "-") {
+        num1 = subtract(num1, num2);
+        setOutput(num1);
+    } else if (operator[0] == "x") {
+        num1 = multiply(num1, num2);
+        setOutput(num1);
+    } else if (operator[0] == "/") {
+        num1 = divide(num1, num2);
+        setOutput(num1);
+    }
+
+    num2 = NaN;
+    operator.shift();
 }
